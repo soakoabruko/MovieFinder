7ce123cf8b3a45e229d9d0e7ec7309ea8b9dcab8
Soako <soakoabruko@gmail.com>
2026-07-07 14:34:14 +0300

    refactor: отредактирован интерфейс API в соответствии с переименованными моделями данных

--- app/src/main/java/com/team/moviefinder/data/api/MovieFinderApi.kt
+++ app/src/main/java/com/team/moviefinder/data/api/MovieFinderApi.kt
@@ -2,19 +2,20 @@ package com.team.moviefinder.data.api
 
 import retrofit2.http.GET
 import retrofit2.http.Path
-import com.team.moviefinder.data.models.Movie
+import com.team.moviefinder.data.models.MovieDetailsResponse
 import retrofit2.http.Query
-import com.team.moviefinder.data.models.MovieResponse
+import com.team.moviefinder.data.models.MovieSearchResponse
 
 interface MovieFinderApi {
     @GET("/api/v2.2/films/{id}")
+    // suspend функция не блокирует текущий поток
     suspend fun getMovieById(
         @Path("id") id: Int,
-    ): Movie // suspend функция не блокирует текущий поток
+    ): MovieDetailsResponse
 
     @GET("/api/v2.1/films/search-by-keyword")
     suspend fun searchMovieByKeywordAndPage(
         @Query("keyword") keyword: String,
         @Query("page") page: Int,
-    ): MovieResponse
+    ): MovieSearchResponse
 }
